ee1fa9a0c4fe9efbf815746bf400a2ac64cec03a
Eric Mok <EricMok@Erics-MacBook-Pro.local>
2016-09-19 20:01:44 -0400

    save1

---
+++ myPseudoCode.js
@@ -0,0 +1,8 @@
+/* My Pseudo-code NYT React Search */
+
+1. Create a [(Mongo) Database] where scrubbed NYT [Articles] will be stored;
+2. Create [Schema] and [Model] for NYT [Articles];
+3. Create function(s) to save scrubbed NYT [Articles];
+4. Create component [Main.js] with <Search> and <Results> functions;
+5. Add component [Child] to [Main.js] for <Saved Results>;
+6. <Saved Results> will change state based on <Results> prop;
